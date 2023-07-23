019bf4ff5586c3a467795d6a6bdad2b71e7d20e3
Michelsom P. da Silva <michelsonpagani@gmail.com>
2023-07-23 12:56:30 -0400

    fix: add graphics and update Dashboard

--- src/screens/DashboardScreen/styles.ts
+++ src/screens/DashboardScreen/styles.ts
@@ -2,39 +2,77 @@ import { Platform } from "react-native";
 import { styled } from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
-  padding: 16px;
-  background-color: #ffffff;
+  align-items: center;
+  height: 100%;
+  background-color: #f9f9f9;
 `;
 export const Header = styled.View`
 width: 100%;
-height: 15%;
+padding: 8% 15px 0px 16px;
+background-color: ${({ theme }) => theme.primary};
+align-items: center;
+flex-direction: row;
+justify-content: space-between;
+`;
+export const Notification = styled.TouchableOpacity`
+width: 48px;
+height: 48px;
+align-items: center;
+justify-content: center;
+border-radius: 8px;
+background-color: ${({ theme }) => theme.greenColor};
+`;
+export const BoxContainer = styled.View`
+width: 60%;
+height:80px;
+padding-top: 15px;
+/* background-color: ${({ theme }) => theme.primary}; */
+flex-direction: column;
 align-items: center;
 justify-content: center;
 `;
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 16px;
   font-weight: bold;
   margin-bottom: 24px;
+  color:#fff;
+  width: 100%;
+  height: 24px;
+
+`;
+
+export const TimetableTitle = styled.Text`
+  font-size: 14px;
+  width: 100%;
+  color: #fff;
+`;
+
+export const UserImage = styled.Image`
+width: 64px;
+height: 64px;
+border-radius: 32px;
+background: #fff;
 `;
 
 export const BalanceContainer = styled.View`
-  background-color: #fff;
-  padding: 16px;
+  margin-top: 20px;
+  width: 100%;
+  background: #fff;
+  justify-content: space-around;
+  align-items: center;
   border-radius: 8px;
-  margin-bottom: 24px;
   ${Platform.select({
   ios: {
-    shadowColor: '#000',
-    shadowOffset: '0px 4px',
+    shadowColor: '#ccc',
+    shadowOffset: '2px 4px',
     shadowOpacity: '0.2',
-    shadowRadius: 16,
-
+    shadowRadius: 15,
   },
   android: {
     elevation: '20',
   },
 })}
+flex-direction: row;
 `;
 
 export const TextEntrance = styled.Text`
